6a38527227ba3d043017eaf27530070761b16b8f
Konstantin Boudnik <cos@boudnik.org>
2016-04-14 13:51:46 -0600

    Replacing source fetching with direct installation of the puppetlabs repo .deb

--- reactive/puppet.py
+++ reactive/puppet.py
@@ -13,6 +13,8 @@ from charmhelpers.core import (
 )
 
 from charms import apt
+import subprocess
+from subprocess import CalledProcessError
 
 
 config = hookenv.config()
@@ -26,6 +28,22 @@ def install_puppet():
     Emits:
     puppet.available: Emitted once the runtime has been installed
     """
+
+    # install required puppet modules
+    # BIGTOP-2003. A workaround to install newer hiera to get rid of hiera 1.3.0 bug.
+    # TODO once Ubuntu trusty fixes version of hiera package, this could be replaced by
+    # adding packages: ['puppet'] in layer.yaml options:basic
+    try:
+        # Ideally the sources should be taken from config.yaml but I can not make it work
+        wget = 'wget -O /tmp/puppetlabs-release-trusty.deb https://apt.puppetlabs.com/puppetlabs-release-trusty.deb'
+        dpkg = 'dpkg -i /tmp/puppetlabs-release-trusty.deb'
+        apt_update = 'apt-get update'
+        puppet_install = 'apt-get install --yes puppet'
+        subprocess.call(wget.split())
+        subprocess.call(dpkg.split())
+    except CalledProcessError:
+        pass  # All modules are set
+
     kv.set('puppet.url', config.get('install_sources'))
     kv.set('puppet.key', config.get('install_keys'))
 
