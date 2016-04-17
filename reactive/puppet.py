0e5f3b771c56fbf4f5b44e74ea8919222e8543dd
Kevin W Monroe <kevin.monroe@canonical.com>
2016-04-17 00:19:25 +0000

    improve comments; let config override puppetlabs repo; fix config changed for empty config opts

--- reactive/puppet.py
+++ reactive/puppet.py
@@ -1,51 +1,46 @@
-import platform
 from charms.reactive import (
-    hook,
     set_state,
     remove_state,
-    main,
     when_not,
     when,
 )
-
-from charmhelpers.core import (
-    hookenv,
-    unitdata,
-)
-
+from charmhelpers.core import hookenv, unitdata
 from charms import apt
+import platform
 import subprocess
-from subprocess import CalledProcessError
 
 
-config = hookenv.config()
-kv = unitdata.kv()
-
-
-@when_not('apt.installed.puppet', 'puppet.available')
+@when_not('apt.installed.puppet')
 def install_puppet():
-    """ Installs latest Puppet & hiera packages
+    """Install Puppet packages.
 
-    Emits:
-    puppet.available: Emitted once the runtime has been installed
+    If the user has configured repos, use them. If not (and we're on trusty),
+    use the puppetlabs repo to avoid buggy archive packages. If neither of
+    these, install packages using the default system repos.
     """
+    config = hookenv.config()
+    kv = unitdata.kv()
+    distribution = platform.linux_distribution()  # looks like [ubuntu, 12.04, precise]
 
-    distribution = platform.linux_distribution()
-    if distribution[2] == 'trusty':
-        # install required puppet modules
+    if config.get('install_sources'):
+        kv.set('puppet.url', config.get('install_sources'))
+        kv.set('puppet.key', config.get('install_keys'))
+        apt.update()
+    elif distribution[2] == 'trusty':
         # BIGTOP-2003. A workaround to install newer hiera to get rid of hiera 1.3.0 bug.
         # TODO once Ubuntu trusty fixes version of hiera package, this could be replaced by
         # adding packages: ['puppet'] in layer.yaml options:basic
-        # Ideally the sources should be taken from config.yaml but I can not make it work
         wget = 'wget -O /tmp/puppetlabs-release-trusty.deb https://apt.puppetlabs.com/puppetlabs-release-trusty.deb'
         dpkg = 'dpkg -i /tmp/puppetlabs-release-trusty.deb'
-        apt_update = 'apt-get update'
-        subprocess.call(wget.split())
-        subprocess.call(dpkg.split())
-        subprocess.call(apt_update.split())
-
-    kv.set('puppet.url', config.get('install_sources'))
-    kv.set('puppet.key', config.get('install_keys'))
+        try:
+            subprocess.check_call(wget.split())
+            subprocess.check_call(dpkg.split())
+        except subprocess.CalledProcessError as e:
+            hookenv.status_set('blocked',
+                               'failed to install puppetlabs repo: %s' % e.output)
+            return False
+        else:
+            apt.update()
 
     apt.queue_install(['puppet'])
     apt.install_queued()
@@ -53,20 +48,28 @@ def install_puppet():
 
 @when('apt.installed.puppet')
 @when_not('puppet.available')
-def puppet_is_set():
-    hookenv.status_set('active', 'Puppet is installed')
+def puppet_ready():
+    """
+    Set status so users know puppet is ready. Set the 'puppet.available' state
+    so other layers can gate puppet operations.
+    """
+    hookenv.status_set('active', 'puppet is installed')
     set_state('puppet.available')
 
 
-@hook('config-changed')
+@when('puppet.available', 'config.changed')
 def version_check():
-    url = config.get('install_sources')
-    key = config.get('install_keys')
+    """
+    If config changed since the previous install, purge the package and remove
+    'puppet.available'. The install_puppet() will run again with the new config.
+    """
+    config = hookenv.config()
+    kv = unitdata.kv()
+
+    # kv.get returns None for empty config; set empty config to None to match
+    url = config.get('install_sources') or None
+    key = config.get('install_keys') or None
 
     if url != kv.get('puppet.url') or key != kv.get('puppet.key'):
         apt.purge(['puppet'])
         remove_state('puppet.available')
-
-
-if __name__ == "__main__":
-    main()
